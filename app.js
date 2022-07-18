a997172d961fbf6f53ce47223a593d5ce46fa936
Muhammad Usama <89479745+rmusamam@users.noreply.github.com>
2022-07-18 12:46:38 +0500

    Insert in DB

--- app.js
+++ app.js
@@ -17,5 +17,38 @@ client.connect(function(err) {
 
   const db = client.db(dbName);
 
-  client.close();
+  insertDocuments(db,()=>{
+    client.close();
+  })
+  
 });
+
+// insert document
+const insertDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('fruits');
+  // Insert some documents
+  collection.insertMany([
+    {
+      name:"apple",
+      score: 6,
+      review: "great fruit"
+    },
+    {
+      name:"mango",
+      score: 7,
+      review: "tasty"
+    },
+    {
+      name:"Banana",
+      score: 9,
+      review: "healthy fruit"
+    }
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.insertedCount);
+    assert.equal(3, Object.keys(result.insertedIds).length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+}
